fdd6c6e8865e46a43e1726a1e251ae884d7409ce
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-02 18:56:31 -0500

    Adding Components

---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// Pascal Naming Convention
+// Means first word is also capitalized
+@Component({
+  // Reference an element named courses
+  selector: 'courses', // <course> "course"
+  template: '<h2>Courses</h2>',
+})
+export class CoursesComponent {
+
+}
