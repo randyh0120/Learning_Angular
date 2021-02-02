67bf151e24ba535d9b8d1bcd50965eb814e45c6b
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-01 21:01:58 -0500

    More TS examples

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -15,8 +15,27 @@
 //   for (let i = 0; i < 5; i++) {
 //     console.log(i);
 //   }
-
+// 
 //   console.log('Finally: ' + i);
 // }
-
+// 
 // doSomething()
+
+//// Declacring lets and types
+// var count = 5;
+// count = 'a';
+// 
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1, 2, 3];
+// let f: any[] = [1, true, 'a', false]
+
+//// Declaring an enem in TS
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 2;
+// 
+// enum Color { Red = 0, Green = 1, Blue = 2, Purple = 3 }
+// let backgroundColor = Color.Red;
