26b9e3e5104a60be59ce36fb04bf6e15388f4a2c
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-01 20:52:31 -0500

    Learning TypeScript

--- ts-hello/dist/out-tsc/main.js
+++ ts-hello/dist/out-tsc/main.js
@@ -1,6 +1,17 @@
-function log(message) {
-    console.log(message);
+// function log(message) {
+//   console.log(message);
+// }
+// var message = "Hello World";
+// log(message);
+//// Declaring Variables
+// var numeber = 1
+// let count = 2
+//// Basic function
+function doSomething() {
+    for (var i = 0; 1 < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
 }
-var message = "Hello World";
-log(message);
+doSomething();
 //# sourceMappingURL=main.js.map
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,7 +1,22 @@
-function log(message) {
-  console.log(message);
-}
+// function log(message) {
+//   console.log(message);
+// }
 
-var message = "Hello World";
+// var message = "Hello World";
 
-log(message);
+// log(message);
+
+//// Declaring Variables
+// var numeber = 1
+// let count = 2
+
+//// Basic function
+// function doSomething() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+
+//   console.log('Finally: ' + i);
+// }
+
+// doSomething()
