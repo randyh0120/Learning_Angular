6711a164e3275becdd46a03cd66cad4256ae76b8
Rhattab0120 <randyhattab.one@gmail.com>
2021-02-02 19:31:30 -0500

    Directive

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -5,10 +5,18 @@ import { Component } from '@angular/core';
 @Component({
   // Reference an element named courses
   selector: 'courses', // <course> "course"
-  template: '<h2>{{ getTitle() }}</h2>',
+  template: `
+  <h2>{{ getTitle() }}</h2>
+  <ul>
+    <li *ngFor="let course of courses">
+      {{ course }}
+    </li>
+  </ul>
+  `,
 })
 export class CoursesComponent {
   title="List of courses"
+  courses = ["course1", "course2", "course3"]
 
   getTitle() {
     return this.title;
